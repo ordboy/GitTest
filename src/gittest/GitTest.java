b21030e70cfa3675d29b388254bf123a9f3b856f
ordboy <stevie.oliphant31@gmail.com>
2018-09-28 13:12:24 +0200

    Test commit22

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -11,6 +11,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hejsan");
         System.out.println("hejsjsjadk");
+        System.out.println("asldnflasdn");
+        System.out.println("sadknfn");
         
     }
 
